636ad72b1de700b56bd91e13b8fa3af653bf7c43
Illich Rada <illich570@gmail.com>
2021-08-31 22:03:54 -0400

    [DEL] remove console.log

--- src/components/Navbar/LinkActive.jsx
+++ src/components/Navbar/LinkActive.jsx
@@ -11,9 +11,6 @@ const LinkActive = ({ href, title }) => {
 	const [isActive,setIsActive] = useState();
 	const classes = useStyles()
 	const router = useRouter()
-	//eslint-disable-next-line
-	console.log(router)
-	
 
 	useEffect(()=>{
 		setIsActive(checkActive(router.pathname, href))
